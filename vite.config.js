ffe0208054e6cb14719652c5010ab418988ce384
Luis Marsiglia <marsiglia.business@gmail.com>
2023-01-25 18:25:29 -0400

    add typescript

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import jsconfigPaths from 'vite-jsconfig-paths'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
-})
